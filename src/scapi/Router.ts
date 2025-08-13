800fac341c933aa741b43eb594ae32a92c6c6a86
KN13KOMETA <kn13kometa@gmail.com>
2025-08-13 23:02:02 +0300

    Writing base for api

---
+++ src/scapi/Router.ts
@@ -0,0 +1,67 @@
+import { match, MatchFunction } from "path-to-regexp";
+import { ServerChannel } from "ssh2";
+
+export interface RouteRequest {
+  options: string;
+  data: object;
+}
+
+export type RouteCallback = (
+  req: RouteRequest,
+  stream: ServerChannel,
+  next: Function | undefined,
+) => {};
+
+export enum RouteType {
+  Router,
+  Middleware,
+  Create,
+  Read,
+  Update,
+  Delete,
+}
+
+export type RouteHandler = RouteCallback | Router;
+
+export interface Route {
+  type: RouteType;
+  path: string;
+  matcher: MatchFunction<object>;
+  handler: RouteHandler;
+}
+
+export class Router {
+  #routes: Route[] = [];
+
+  constructor() {}
+
+  input(exec: string) {}
+
+  #addRoute(type: RouteType, path: string, handler: RouteHandler) {
+    this.#routes.push({
+      type,
+      path,
+      matcher: match(path, { decode: decodeURIComponent }),
+      handler,
+    });
+  }
+
+  use(path: string, handler: RouteHandler) {
+    if (handler instanceof Router)
+      this.#addRoute(RouteType.Router, path, handler);
+    else this.#addRoute(RouteType.Middleware, path, handler);
+  }
+
+  create(path: string, cb: RouteCallback) {
+    this.#addRoute(RouteType.Create, path, cb);
+  }
+  read(path: string, cb: RouteCallback) {
+    this.#addRoute(RouteType.Read, path, cb);
+  }
+  update(path: string, cb: RouteCallback) {
+    this.#addRoute(RouteType.Update, path, cb);
+  }
+  delete(path: string, cb: RouteCallback) {
+    this.#addRoute(RouteType.Delete, path, cb);
+  }
+}
