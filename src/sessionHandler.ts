10d0c4b3ac0f4265fbc007f1c24542170d640f4f
KN13KOMETA <kn13kometa@gmail.com>
2025-08-21 18:31:14 +0300

    Better error handle, no stuck connections on error

--- src/sessionHandler.ts
+++ src/sessionHandler.ts
@@ -33,14 +33,18 @@ export default (
 
     const commandName = info.command.split(" ", 1)[0];
 
-    if (commandName == "api")
+    if (commandName == "api") {
+      const stream = accept();
       try {
-        api.inputRaw(info.command, { userId }, accept());
+        api.inputRaw(info.command, { userId }, stream);
       } catch (error) {
         logger.error(error);
+        if (typeof error == "string" || error instanceof Error)
+          stream.write(error.toString());
+        stream.exit(1);
+        stream.end();
       }
-
-    reject();
+    } else reject();
   });
 
   session.on("sftp", (accept, reject) => {
