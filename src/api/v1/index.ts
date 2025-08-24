53fbfb331e108f86f13b8954cc171eb5e3cb2624
KN13KOMETA <kn13kometa@gmail.com>
2025-08-25 01:10:16 +0300

    Move validation in function

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,21 +2,32 @@ import pino from "pino";
 import { PrismaClient } from "../../generated/prisma/client";
 import { Router } from "../../scapi";
 import ErrorResponse, { ResponseCode } from "../ErrorResponse";
+import { ServerChannel } from "ssh2";
+
+const validatePositiveNumber = (
+  name: string,
+  x: any,
+  stream: ServerChannel,
+) => {
+  if (Number.isSafeInteger(x) && x >= 0) return true;
+
+  const res = new ErrorResponse(ResponseCode.BadRequest, {
+    message: name + " must positive number",
+    data: null,
+  });
+  stream.write(res.toString());
+  stream.exit(res.code);
+  stream.end();
+
+  return false;
+};
 
 export default (prisma: PrismaClient, logger?: pino.Logger): Router => {
   const v1 = new Router(logger);
 
   v1.read("/users/:userId", async (req, stream) => {
     const id = Number(req.params.userId);
-    if (!Number.isSafeInteger(id) || id < 0) {
-      const res = new ErrorResponse(ResponseCode.BadRequest, {
-        message: "userId must positive number",
-        data: null,
-      });
-      stream.write(res.toString());
-      stream.exit(res.code);
-      return stream.end();
-    }
+    if (!validatePositiveNumber("userId", id, stream)) return;
 
     const user = await prisma.user.findUnique({ where: { id } });
     stream.write(JSON.stringify(user));
