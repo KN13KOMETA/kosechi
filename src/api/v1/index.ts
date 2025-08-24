1ba089d0b76cfddae5a7c62b1a1cf644b276e8f0
KN13KOMETA <kn13kometa@gmail.com>
2025-08-25 00:32:06 +0300

    Remove useless check

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -7,16 +7,6 @@ export default (prisma: PrismaClient, logger?: pino.Logger): Router => {
   const v1 = new Router(logger);
 
   v1.read("/users/:userId", async (req, stream) => {
-    if (typeof req.params.userId != "string") {
-      const res = new ErrorResponse(ResponseCode.BadRequest, {
-        message: "userId must be string",
-        data: null,
-      });
-      stream.write(res.toString());
-      stream.exit(res.code);
-      return stream.end();
-    }
-
     const id = Number(req.params.userId);
     if (!Number.isSafeInteger(id) || id < 0) {
       const res = new ErrorResponse(ResponseCode.BadRequest, {
