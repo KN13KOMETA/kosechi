095795ccc73978b74016f0248e34ba068b7af97b
KN13KOMETA <kn13kometa@gmail.com>
2025-08-28 23:16:46 +0300

    Add checkSelect and validateSelect

--- src/api/v1/fun.ts
+++ src/api/v1/fun.ts
@@ -1,14 +1,46 @@
 import { ServerChannel } from "ssh2";
 import ErrorResponse, { ResponseCode } from "../ErrorResponse";
 
-export const checkPositiveNumber = (x: any) =>
+export const checkSelect = (select: any, whitelist: string[]): boolean => {
+  if (typeof select != "object") return false;
+
+  for (const key in select)
+    if (
+      !whitelist.includes(key) ||
+      (typeof select[key] != "number" && typeof select[key] != "boolean")
+    )
+      return false;
+
+  return true;
+};
+
+export const validateSelect = (
+  select: any,
+  whitelist: string[],
+  stream: ServerChannel,
+): boolean => {
+  if (checkSelect(select, whitelist)) return true;
+
+  const res = new ErrorResponse(ResponseCode.BadRequest, {
+    message: "select is invalid",
+    data: null,
+  });
+
+  stream.write(res.toString());
+  stream.exit(res.code);
+  stream.end();
+
+  return false;
+};
+
+export const checkPositiveNumber = (x: any): boolean =>
   Number.isSafeInteger(x) && x >= 0;
 
 export const validatePositiveNumber = (
   name: string,
   x: any,
   stream: ServerChannel,
-) => {
+): boolean => {
   if (checkPositiveNumber(x)) return true;
 
   const res = new ErrorResponse(ResponseCode.BadRequest, {
