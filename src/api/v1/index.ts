77b5691117b3fe61a2dcad02cda7c5d8d069ab91
KN13KOMETA <kn13kometa@gmail.com>
2025-08-28 16:32:01 +0300

    Get user and permissions in middleware

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,8 +1,10 @@
 import pino from "pino";
-import { PrismaClient } from "../../generated/prisma/client";
-import { Router } from "../../scapi";
+import { Prisma, PrismaClient } from "../../generated/prisma/client";
+import { Command, Router, RouteRequest } from "../../scapi";
 import ErrorResponse, { ResponseCode } from "../ErrorResponse";
 import { ServerChannel } from "ssh2";
+import UserPermissions from "../UserPermissions";
+import { ParamData } from "path-to-regexp";
 
 const checkPositiveNumber = (x: any) => Number.isSafeInteger(x) && x >= 0;
 const validatePositiveNumber = (
@@ -23,19 +25,81 @@ const validatePositiveNumber = (
   return false;
 };
 
+export interface UserRouteRequest<Params = ParamData> {
+  params: Params;
+  data: {
+    userId: number;
+    userPerms: UserPermissions;
+    user: Prisma.UserGetPayload<{
+      include: {
+        roles: {
+          select: {
+            id: true;
+            order: true;
+            permissions: true;
+          };
+        };
+      };
+    }>;
+  };
+  cmd: Command;
+}
+
 export default (prisma: PrismaClient, logger?: pino.Logger): Router => {
   const v1 = new Router(logger);
 
-  v1.use((req, stream, next) => {
-    if (typeof req.data.userId == "number") return next();
+  v1.use(async (req, stream, next) => {
+    if (typeof req.data.userId != "number") {
+      const res = new ErrorResponse(ResponseCode.ImATeapot, {
+        message: "Internal Server Error: userId doesn't exists",
+        data: null,
+      });
+      stream.write(res.toString());
+      stream.exit(res.code);
+      stream.end();
+      return;
+    }
 
-    const res = new ErrorResponse(ResponseCode.ImATeapot, {
-      message: "Internal Server Error: userId doesn't exists",
-      data: null,
+    const user = await prisma.user.findUnique({
+      where: { id: req.data.userId },
+      include: {
+        roles: {
+          select: {
+            id: true,
+            order: true,
+            permissions: true,
+          },
+        },
+      },
     });
-    stream.write(res.toString());
-    stream.exit(res.code);
-    stream.end();
+    if (user == null) {
+      const res = new ErrorResponse(ResponseCode.ImATeapot, {
+        message: "Internal Server Error: user doesn't exists",
+        data: null,
+      });
+      stream.write(res.toString());
+      stream.exit(res.code);
+      stream.end();
+      return;
+    }
+
+    const userPerms = new UserPermissions();
+    const tPerms = new UserPermissions();
+
+    for (const role of user.roles) {
+      tPerms.perms = role.permissions;
+      if (tPerms.manageChannels) userPerms.manageChannels = true;
+      if (tPerms.manageRoles) userPerms.manageRoles = true;
+      if (tPerms.inviteUsers) userPerms.inviteUsers = true;
+      if (tPerms.manageUsers) userPerms.manageUsers = true;
+      if (tPerms.admin) userPerms.admin = true;
+      if (tPerms.superadmin) userPerms.superadmin = true;
+    }
+
+    req.data.user = user;
+    req.data.userPerms = userPerms;
+
+    return next();
   });
 
   v1.read("/users", async (req, stream) => {
@@ -76,7 +140,7 @@ export default (prisma: PrismaClient, logger?: pino.Logger): Router => {
     stream.end();
   });
 
-  v1.read("/users/:userId", async (req, stream) => {
+  v1.read("/users/:userId", async (req: UserRouteRequest, stream) => {
     const id = Number(req.params.userId);
     if (!validatePositiveNumber("userId", id, stream)) return;
 
